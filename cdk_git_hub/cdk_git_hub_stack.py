e8f16ccfab8b91728873b5a98b7c53d5d7839799
Amit Kumar Nag <amitkumarnag@Amits-MBP.fritz.box>
2023-02-20 20:06:47 +0100

    commit for creation of the resources

--- cdk_git_hub/cdk_git_hub_stack.py
+++ cdk_git_hub/cdk_git_hub_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_lambda as lambda_function
 )
 from constructs import Construct
 
@@ -11,9 +13,19 @@ class CdkGitHubStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        #example resource
+        queue = sqs.Queue(
+             self, "CdkGitHubQueue",
+             visibility_timeout=Duration.seconds(300),
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkGitHubQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(self, "MyfirstBucket", versioned=True,
+                           bucket_name="demo-bucket-beyond-the-cloud-98979867",
+                           block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+
+        function = lambda_function.Function(self, "DemoCDKFunction",
+                                            function_name="cdk_github_demo",
+                                            runtime=lambda_function.Runtime.PYTHON_3_9,
+                                            code=lambda_function.Code.from_asset('./lambda_code_demo'),
+                                            handler="demo_lambda.lambda_handler")
+
